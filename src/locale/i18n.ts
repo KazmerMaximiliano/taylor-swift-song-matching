4faf38301b6f9c962868c423bd2216e0d2ce1652
mkazmer <mkazmer@getfivvy.com>
2024-05-23 18:14:49 -0300

    UPDATE auto detect language

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -1,13 +1,15 @@
 import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 import enJSON from './en.json';
 import esJSON from './es.json';
 
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { ...enJSON },
-    es: { ...esJSON },
-  },
-  lng: "es",
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { ...enJSON },
+      es: { ...esJSON },
+    },
+  });
